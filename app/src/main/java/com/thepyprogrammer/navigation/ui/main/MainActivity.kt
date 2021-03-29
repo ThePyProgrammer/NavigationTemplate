0ac2dfec73c6e652e47140f99d1d63e465c6e81a
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-30 00:08:28 +0800

    Update Everything All Over Again

--- app/src/main/java/com/thepyprogrammer/navigation/ui/main/MainActivity.kt
+++ app/src/main/java/com/thepyprogrammer/navigation/ui/main/MainActivity.kt
@@ -89,7 +89,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_profile, R.id.nav_settings
+                R.id.nav_home, R.id.nav_profile, R.id.nav_list, R.id.nav_settings
             ), drawer_layout
         )
 
